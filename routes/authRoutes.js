550f0db3c72247d931e390979fd2fd2a3ea435b8
renegadefloofer <566976656b@gmail.com>
2022-01-03 05:26:37 -0500

    add prod and dev env variables

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,4 +11,13 @@ module.exports = (app) => {
 
   //handling the callback after google auth redirects to providede URL
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
 };
